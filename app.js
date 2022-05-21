472952f51853977f8529cf54105bb475fdc64ec6
enzosv <vergara.enzo@gmail.com>
2022-05-21 12:27:45 +0800

    cleanup

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
+const HOST = "bionic-reading1.p.rapidapi.com";
 async function main(content, key) {
   localStorage.setItem("key", key);
-
+  let old = localStorage.getItem("content");
+  if (old == content) {
+    return;
+  }
+  localStorage.setItem("conent", content);
   if (content.length < 1) {
     return;
   }
   const button = document.getElementById("convert");
   button.disabled = true;
+  const text = await convert(content, key);
+  button.disabled = false;
+  document.getElementById("response").innerHTML = text;
+}
+
+async function convert(content, key) {
   const encodedParams = new URLSearchParams();
   encodedParams.append("content", content);
   encodedParams.append("response_type", "html");
   encodedParams.append("request_type", "html");
 
   // TODO: Adjust based on view size
-  encodedParams.append("fixation", "1");
-  encodedParams.append("saccade", "10");
+  encodedParams.append("fixation", "1"); // 1-5 steps of 1
+  encodedParams.append("saccade", "10"); // 10-50 steps of 10
 
   const options = {
     method: "POST",
     headers: {
       "content-type": "application/x-www-form-urlencoded",
-      "X-RapidAPI-Host": "bionic-reading1.p.rapidapi.com",
+      "X-RapidAPI-Host": HOST,
       "X-RapidAPI-Key": key,
     },
     body: encodedParams,
   };
 
-  const response = await fetch(
-    "https://bionic-reading1.p.rapidapi.com/convert",
-    options
-  );
-  button.disabled = false;
-  document.getElementById("response").innerHTML = await response.text();
+  const response = await fetch(`https://${HOST}/convert`, options);
+  return await response.text();
 }
 
 document.getElementById("api_key").value = localStorage.getItem("key");
